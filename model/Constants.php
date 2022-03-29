eb987362c31f07b203d751ecdd1326d056f74990
ingridygama <86685202+ingridygama@users.noreply.github.com>
2022-03-29 15:58:58 -0300

    Update Constants.php

--- model/Constants.php
+++ model/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','silvioflorentino.mysql.database.azure.com');
-define('DB_USER','silvioflorentino@silvioflorentino');
-define('DB_PASS','1978@|Silvio');
+define('DB_HOST','ingridygama.mysql.database.azure.com');
+define('DB_USER','ingridygama@ingridygama');
+define('DB_PASS','Gama007981@');
 define('DB_NAME','hortfruitbd');
